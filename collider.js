89e928354313ea822ec267528a4de16af87a8b0d
rcerf <rickcerf@gmail.com>
2013-10-04 21:33:11 -0700

    player can now move with clicks

--- collider.js
+++ collider.js
@@ -2,7 +2,7 @@
   var gameOptions = {
     width: 700,
     height: 450,
-    nEnemies: 30,
+    nEnemies: 10,
     padding: 20
   };
 
@@ -16,8 +16,43 @@
     y: d3.scale.linear().domain([0, 100]).range([0, gameOptions.height])
   };
 
-  var gameBoard = d3.select('.container').append('svg').attr('width', gameOptions.width)
+  var gameBoard = d3.select('.container').append('svg:svg').attr('width', gameOptions.width)
   .attr('height', gameOptions.height);
+
+  var createPlayer = function(){
+    return _.range(0, 1).map(function(player){
+      return {
+        id: player,
+        x: 50,
+        y: 50
+      };
+    });
+  };
+
+  // var playerArray = [];
+  // playerArray.push(player);
+  // console.log(player);
+
+  var renderPlayer = function(playerArray){
+    var playerData = gameBoard.selectAll('circle.player').data(playerArray, function(player){
+      return player.id;
+    });
+
+
+    playerData.enter().append('svg:circle').attr('class', 'player').attr('cx', function(player){
+      return axes.x(player.x);
+    }).attr('cy', function(player){
+      return axes.y(player.y);
+    }).attr('r', 10).attr('fill', 'aqua');
+
+    d3.select("svg").on("click", function(){
+      d3.select(".player").transition().duration(500).attr('cx', function(){
+        return d3.mouse(this)[0];
+        }).attr('cy', function(){
+          return d3.mouse(this)[1];
+      });
+    });
+  };
 
   var render = function(newEnemies){
     var enemyBoard = gameBoard.selectAll('circle.enemy').data(newEnemies, function(d){
@@ -29,6 +64,8 @@
     }).attr('cy', function(enemy){
       return axes.y(enemy.y);
     }).attr('r', 10);
+
+    
 
     setInterval(function(){
       var newEnemyLocation = createEnemies();
@@ -51,7 +88,8 @@
       };
     });
   };
-
+var newPlayer = createPlayer();
+renderPlayer(newPlayer);
 var newEnemies = createEnemies();
 render(newEnemies);
 // setInterval(function(){
@@ -59,5 +97,8 @@ render(newEnemies);
 //   var newEnemypositions = createEnemies();
 //   return render(newEnemypositions);
 // }, 1000);
+
+
+
 
 }).call(this);
